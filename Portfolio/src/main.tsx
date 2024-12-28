b83d1970ce8381b3ca69c17782636eb0ca1c42e4
MahenoorAhsan <mahenoor.ahsan@aot.edu.in>
2024-12-28 15:00:41 +0530

    hello world changes

--- Portfolio/src/main.tsx
+++ Portfolio/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource/outfit";
+import "@fontsource/roboto";
 import './index.css'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <StrictMode>so
     <App />
   </StrictMode>,
 )
