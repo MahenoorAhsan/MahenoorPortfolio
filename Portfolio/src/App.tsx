6825b352f6441a396c52c944c48bb019792ed2ec
Mahenoor Ahsan <mahii828113@gmail.com>
2025-02-10 00:08:04 +0530

    image added

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,11 +1,13 @@
 
 import styles from './App.module.css'
+import { Hero } from './components/Navbar/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className={styles.app}>
       <Navbar />
+      <Hero />
     </div>
   )
 }
--- Portfolio/src/components/Navbar/Hero/Hero.tsx
+++ Portfolio/src/components/Navbar/Hero/Hero.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { getImageUrl } from "../../../utils";
+import styles from './Hero.module.css';
 
 export const Hero = () => {
-    return <section>
-        <div>
-            <h1> I am Mahenoor Ahsan!</h1>
-            <p>I am a software Developer with almost 2 years of experience in Java and Angular.Reach out, if you wanna know more.</p>
-            <a href="mailto:mahii828113@gmail.com">Contact Me !</a>
+    return <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}> I am Mahenoor Ahsan!</h1>
+            <p className={styles.description}>I am a software Developer with 1.5 years of experience in Java and Angular. Reach out, if you wanna know more.</p>
+            <a  className={styles.contactBtn} href="mailto:mahii828113@gmail.com">Contact Me !</a>
         </div>
+        <img src='../../../assets/myPic.png' alt="Her" className={styles.heroImg}/>
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
     </section>
 }
