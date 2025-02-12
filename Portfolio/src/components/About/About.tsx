9c95b738f8b1e247da96e3053dbfa26e59f82633
Mahenoor Ahsan <mahii828113@gmail.com>
2025-02-12 22:23:13 +0530

    about section complete

--- Portfolio/src/components/About/About.tsx
+++ Portfolio/src/components/About/About.tsx
@@ -3,37 +3,40 @@ import styles from './About.module.css'
 
 export const About= ()=>{
 
-    return <section className={styles.container}>
+    return <section className={styles.container} id='about'>
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}>
             <img src="../../../assets/girlWithLaptop.jpg" alt="me sitting with a laptop"
             className={styles.aboutImage} />
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
-                    <img src="cusrsor.png" alt="Cursor"/>
+                    <img src="../../assets/frontend.png" alt="Cursor" className={styles.icon}/>
                     <div className={styles.aboutItemText}>
                         <h3>Frontend Developer</h3>
                         <p>I'm a frontend developer with a experience in building responsive
-                            and optimized sites.
+                            and optimized sites.slkwhdhroifewhroiwehioen buwFUW BUiwebrw eigfueif veyvfy vyeufyewf vieYFY huewiu uegye jyegfyuew yfyuw vewyufvu vuyvf
+                            vueywfugy if deivwy iqwef fhrvf bekwfu
                         </p>
                     </div>
                 </li>
                 <li className={styles.aboutItem}>
-                    <img src="server.png" alt="Server"/>
+                    <img src="../../assets/backend.jpg" alt="Server" className={styles.icon}/>
                     <div className={styles.aboutItemText}>
                         <h3>Backend Developer</h3>
                         <p>I have experience developing fast and optimized backend systems and APIs.
+                        slkwhdhroifewhroiwehioen buwFUW BUiwebrw eigfueif veyvfy vyeufyewf vieYFY huewiu uegye jyegfyuew yfyuw vewyufvu vuyvf
+                        vueywfugy if deivwy iqwef fhrvf bekwfu
                         </p>
                     </div>
                 </li>
-                <li className={styles.aboutItem}>
+                {/* <li className={styles.aboutItem}>
                     <img src="design.png" alt="Cursor"/>
                     <div className={styles.aboutItemText}>
                         <h3>UI Designer</h3>
                         <p>I have design multiple landing pages and have systems as well
                         </p>
                     </div>
-                </li>
+                </li> */}
             </ul>
         </div>
     </section>
